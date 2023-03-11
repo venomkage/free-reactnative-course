cea2ac2dbe0a70b8133d610e9a0d542401875cee
venomkage <ceomindset3@gmail.com>
2023-03-11 12:37:58 +0530

    Important elements

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {
+  Button,
+  FlatList,
+  Image,
+  Pressable,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import SinglePost from './src/Components/SinglePost';
 
+// Flatlist
+// ScrollView
+// Touchables - TouchableOpacity, ...etc
+// Pressable
+// Images
+
 const Data = [
+  {
+    fname: 'Hey',
+  },
+  {
+    fname: 'Hii',
+  },
+  {
+    fname: 'How',
+  },
   {
     fname: 'Hey',
   },
@@ -18,18 +42,50 @@ export default function App() {
   const [number, setNumber] = useState(0);
 
   return (
-    <View
-      style={{
-        backgroundColor: 'black',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      {Data.map((item, index) => {
-        return <SinglePost key={index} fname={item.fname} />;
-      })}
+    // <ScrollView
+    //   style={{
+    //     backgroundColor: 'black',
+    //     flex: 1,
+    //   }}
+    //   contentContainerStyle={{
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //   }}>
+    //   {Data.map((item, index) => {
+    //     return <SinglePost key={index} fname={item.fname} />;
+    //   })}
 
-      <Button title="Press me" onPress={() => setNumber(number + 1)} />
+    //   <Button title="Press me" onPress={() => setNumber(number + 1)} />
+    // </ScrollView>
+    <View style={{flex: 1}}>
+      <FlatList
+        data={Data}
+        renderItem={({item, index}) => {
+          return <SinglePost key={index} fname={item.fname} />;
+        }}
+      />
+      <Pressable
+        style={{
+          backgroundColor: 'black',
+          width: '50%',
+          alignSelf: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          // padding: 6,
+          borderRadius: 3,
+          marginBottom: 3,
+          height: 30,
+        }}
+        onPress={() => console.log('first')}>
+        <Text style={{color: 'white'}}>Press Me</Text>
+      </Pressable>
+      <Image
+        source={require('./src/Components/rose.jpg')}
+        style={{
+          height: 150,
+          width: 150,
+        }}
+      />
     </View>
   );
 }
